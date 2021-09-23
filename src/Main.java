07f265785b04c9218f9f372742895d9836e5888c
Lina <llazareva7708@gmail.com>
2021-09-23 16:15:45 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int priceTicket = 2015;
+        int countTicket = 2;
+        int miles = (priceTicket * countTicket) / 20;
+        System.out.println(miles);
+
+    }
+}
+
